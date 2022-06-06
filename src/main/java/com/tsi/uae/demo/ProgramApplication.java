efeb58dd4ca44c758bcd5210064ce00c8c079194
alexchimzi <chilaeze@gmail.com>
2022-06-06 20:14:21 +0100

    first working version no error

--- src/main/java/com/tsi/uae/demo/ProgramApplication.java
+++ src/main/java/com/tsi/uae/demo/ProgramApplication.java
@@ -11,21 +11,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/Home")
 public class ProgramApplication {
 	@Autowired
-	private @Qualifier("actors") ActorRepository actorRepository;
+	private @Qualifier("actor") ActorRepository actorRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgramApplication.class, args);
 	}
 
-	public ProgramApplication(@Qualifier("actors") ActorRepository actorRepository){
+	public ProgramApplication(@Qualifier("actor") ActorRepository actorRepository){
 		this.actorRepository = actorRepository;
 	}
-
-	@PostMapping("/Add_New_Actor")
-	public @ResponseBody String addNewUser (@RequestParam String first_name,@RequestParam String last_name){
-		Actor a = new Actor();
-
-		actorRepository.save(a);
-		return "";
+	@GetMapping("/Get_Actors")
+	public @ResponseBody Iterable<Actor>getAllActors(){
+		return actorRepository.findAll();
 	}
+	//@PostMapping("/Add_New_Actor")
+	//public @ResponseBody Iterable<Actor>post_(){}
+
+
 }
